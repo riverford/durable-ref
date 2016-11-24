fa448fe9e679072c0156d5721105e400da33b440
Dan Stone <danielstone@riverford.co.ukls>
2016-11-24 09:40:04 +0000

    interning of value references working, added readonly ref

--- src/riverford/durable_ref/core.clj
+++ src/riverford/durable_ref/core.clj
@@ -1,8 +1,8 @@
 (ns riverford.durable-ref.core
-  "Provides a DurableRef and DurableValueRef. Both a derefable reference types
+  "Provides a DurableVolatileRef and DurableValueRef. Both a derefable reference types
   that refer to clojure data potentially off-heap.
 
-  DurableRef represents a durable mutable cell. Create with `reference` on a valid URI.
+  DurableVolatileRef represents a durable mutable cell. Create with `reference` on a valid URI.
   DurableValueRef represents a content-addressed durable immutable cell. Create with `persist!` with a valid base-uri
   and optional format.
 
@@ -10,8 +10,8 @@
   that takes a URI.
 
   The uri scheme should follow like so:
-  `dref:mem://foo/bar/baz.edn`
-  `dvref:mem://foo/bar/f79975f99908e5387fca3b503d9e9adbefaafc5e.edn.zip`
+  `mutable-ref:mem://foo/bar/baz.edn`
+  `ref:mem://foo/bar/f79975f99908e5387fca3b503d9e9adbefaafc5e.edn.zip`
 
   Where `mem` is a subscheme that describes how to read and write bytes to a location described by the URI.
   `mem` & `file` are schemes supported by default. See the scheme.* namespaces for extra schemes.
@@ -30,9 +30,14 @@
            (java.util.zip GZIPInputStream GZIPOutputStream)
            (java.security MessageDigest DigestInputStream)
            (javax.xml.bind DatatypeConverter)
-           (java.io ByteArrayOutputStream PushbackReader ByteArrayInputStream)
+           (java.io ByteArrayOutputStream PushbackReader)
            (java.util.concurrent ConcurrentHashMap)
-           (clojure.lang IDeref)))
+           (clojure.lang IDeref)
+           (java.util WeakHashMap)
+           (java.lang.ref WeakReference)))
+
+(comment
+  (set! *warn-on-reflection* true))
 
 (defmulti serialize (fn [obj format] format))
 (defmulti deserialize (fn [in format] format))
@@ -108,34 +113,37 @@
   (throw (IllegalArgumentException. (format "Not a deletable ref scheme %s" (.getScheme uri)))))
 
 (defprotocol IDurableRef
-  (-deref [this opts]))
+  (-deref [this opts])
+  (-props [this]))
 
-(deftype DurableRef [^URI uri]
+(deftype DurableVolatileRef [^URI uri]
   IDurableRef
   (-deref [this opts]
     (when-some [bytes (read-bytes (.getSchemeSpecificPart uri) opts)]
       (deserialize-from bytes uri)))
+  (-props [this]
+    {:uri uri
+     :read-only? false
+     :cacheable? false})
   IDeref
   (deref [this]
     (-deref this {}))
   Object
   (equals [this obj]
-    (and (instance? DurableRef obj)
-         (= uri (.-uri ^DurableRef obj))))
+    (and (instance? DurableVolatileRef obj)
+         (= uri (.-uri ^DurableVolatileRef obj))))
   (hashCode [this]
-    (.hashCode uri)))
+    (.hashCode uri))
+  (toString [this]
+    (str uri)))
 
-(defmethod print-method DurableRef
-  [obj w]
-  (.write w (str "#dref \"" (.-uri ^DurableRef obj) "\"")))
-
-(defn- mutable-ref
+(defn- volatile-ref
   [uri]
   (let [uri (URI. (str/lower-case (str uri)))]
-    (assert (= "dref" (.getScheme ^URI uri)) "Invalid URI, scheme must be dref.")
-    (->DurableRef uri)))
+    (assert (= "volatile" (.getScheme ^URI uri)) "Invalid URI, scheme must be volatile.")
+    (->DurableVolatileRef uri)))
 
-(defn- sha1
+(defn- hash-identity
   ([x]
    (let [digest (MessageDigest/getInstance "sha1")]
      (with-open [in (io/input-stream x)
@@ -147,31 +155,37 @@
   [bytes]
   (DatatypeConverter/printHexBinary bytes))
 
-(def ^:dynamic *perform-checksum-check*
+(def ^:dynamic *verify-hash-identity*
   true)
 
-(deftype DurableValueRef [^URI uri ^:volatile-mutable _val]
+(deftype DurableValueRef [uri ^:volatile-mutable _val]
   IDurableRef
   (-deref [this opts]
     (cond
       (= ::nil _val) nil
       (some? _val) _val
-      :else (let [sub-uri (URI. (.getSchemeSpecificPart uri))
-                  path (.getPath sub-uri)
-                  bytes (read-bytes sub-uri opts)]
-              (if (nil? bytes)
-                (throw (NullPointerException. "DurableValueRef points to nothing. Storage may have been mutated."))
-                (if (or (not *perform-checksum-check*)
-                        (= (str/lower-case (hex-encode (sha1 bytes)))
-                           (some-> (str/split path #"/")
-                                   last
-                                   (str/split #"\.")
-                                   first
-                                   str/lower-case)))
-                  (let [v (deserialize-from bytes sub-uri)]
-                    (set! _val v)
-                    v)
-                  (throw (IllegalStateException. "DurableValueRef checksum mismatch. Storage may have been mutated.")))))))
+      :else (locking this
+              (let [sub-uri (URI. (.getSchemeSpecificPart ^URI uri))
+                    path (.getPath sub-uri)
+                    bytes (read-bytes sub-uri opts)]
+                (if (nil? bytes)
+                  (throw (NullPointerException. "DurableValueRef points to nothing. Storage may have been mutated."))
+                  (if (or (not *verify-hash-identity*)
+                          (= (str/lower-case (hex-encode (hash-identity bytes)))
+                             (some-> (str/split path #"/")
+                                     last
+                                     (str/split #"\.")
+                                     first
+                                     str/lower-case)))
+                    (let [v (deserialize-from bytes sub-uri)]
+                      (set! _val v)
+                      v)
+                    (throw (IllegalStateException. "DurableValueRef checksum mismatch. Storage may have been mutated."))))))))
+  (-props [this]
+    {:uri uri
+     :read-only? true
+     :cacheable? true
+     :cache-hot? (some? _val)})
   IDeref
   (deref [this]
     (-deref this {}))
@@ -180,18 +194,16 @@
     (and (instance? DurableValueRef obj)
          (= uri (.-uri ^DurableValueRef obj))))
   (hashCode [this]
-    (.hashCode uri)))
-
-(defmethod print-method DurableValueRef
-  [obj w]
-  (.write w (str "#dvref \"" (.-uri ^DurableValueRef obj) "\"")))
+    (.hashCode uri))
+  (toString [this]
+    (str uri)))
 
 (defn- value-ref
   [uri]
   (let [uri (URI. (str/lower-case (str uri)))]
-    (when (not= "dvref" (.getScheme ^URI uri))
+    (when (not= "value" (.getScheme ^URI uri))
       (throw (IllegalArgumentException.
-               (format "Invalid URI, scheme must be dvref (got %s)" (.getScheme uri)))))
+               (format "Invalid URI, scheme must be value (got %s)" (.getScheme uri)))))
     (->DurableValueRef uri nil)))
 
 (defn persist!
@@ -213,7 +225,7 @@
        (throw (IllegalArgumentException. (clojure.core/format "No write-bytes! impl for uri scheme (%s)"
                                                               scheme))))
      (let [bytes (serialize obj format)
-           sha1 (sha1 bytes)
+           sha1 (hash-identity bytes)
            base-uri (str base-uri)
            uri (URI.
                  (str/lower-case
@@ -223,66 +235,129 @@
                         "."
                         format)))]
        (write-bytes! uri bytes (-> opts :scheme-opts (get scheme)))
-       (->DurableValueRef (str "dvref:" uri) (if (nil? obj) ::nil obj))))))
+       (->DurableValueRef (str "value:" uri) (if (nil? obj) ::nil obj))))))
+
+(deftype DurableReadonlyRef [uri]
+  IDurableRef
+  (-deref [this opts]
+    (when-some [bytes (read-bytes uri opts)]
+      (deserialize-from bytes uri)))
+  (-props [this]
+    {:uri uri
+     :read-only? true
+     :cacheable? false})
+  IDeref
+  (deref [this]
+    (-deref this {}))
+  Object
+  (equals [this obj]
+    (and (instance? DurableReadonlyRef obj)
+         (= uri (.-uri ^DurableReadonlyRef obj))))
+  (hashCode [this]
+    (.hashCode uri))
+  (toString [this]
+    (str uri)))
+
+(defmethod print-method riverford.durable_ref.core.IDurableRef
+  [o w]
+  (.write w (str "#object [" (.getName (class o)) " \"" o "\"]")))
+
+(prefer-method print-method riverford.durable_ref.core.IDurableRef IDeref)
+
+(def ^:private intern-val
+  (let [pool (WeakHashMap.)]
+    (fn [obj]
+      (locking pool
+        (loop [ret nil]
+          (if (some? ret)
+            ret
+            (let [wref (get pool obj)]
+              (if (nil? wref)
+                (do (.put pool obj (WeakReference. obj))
+                    obj)
+                (recur (.get ^WeakReference wref))))))))))
 
 (defn reference
-  "Returns a durable ref or durable value ref for the URI according to the scheme."
+  "Returns a ref for the URI according to the scheme.
+   e.g value:mem:/foo/bar."
   [uri]
   (let [uri (URI. (str uri))
         scheme (.getScheme uri)
         sub-scheme (.getScheme (URI. (.getSchemeSpecificPart uri)))]
+    (if (nil? sub-scheme)
+      (do
+        (when (nil? (get-method read-bytes scheme))
+          (throw (IllegalArgumentException. (clojure.core/format "No read-bytes impl for uri scheme (%s)"
+                                                                 scheme))))
 
-    (when (nil? (get-method read-bytes sub-scheme))
-      (throw (IllegalArgumentException. (clojure.core/format "No read-bytes impl for uri scheme (%s)"
-                                                             sub-scheme))))
+        (when (nil? (get-method write-bytes! scheme))
+          (throw (IllegalArgumentException. (clojure.core/format "No write-bytes! impl for uri scheme (%s)"
+                                                                 scheme))))
 
-    (when (nil? (get-method write-bytes! sub-scheme))
-      (throw (IllegalArgumentException. (clojure.core/format "No write-bytes! impl for uri scheme (%s)"
-                                                             sub-scheme))))
+        (->DurableReadonlyRef uri))
+      (do
+        (when (nil? (get-method read-bytes sub-scheme))
+          (throw (IllegalArgumentException. (clojure.core/format "No read-bytes impl for uri scheme (%s)"
+                                                                 sub-scheme))))
 
-    (case scheme
-      "dref" (mutable-ref uri)
-      "dvref" (value-ref uri)
-      (throw (IllegalArgumentException. (format "Invalid reference scheme %s" scheme))))))
+        (when (nil? (get-method write-bytes! sub-scheme))
+          (throw (IllegalArgumentException. (clojure.core/format "No write-bytes! impl for uri scheme (%s)"
+                                                                 sub-scheme))))
+
+        (case scheme
+          "volatile" (volatile-ref uri)
+          "value" (intern-val (value-ref uri))
+
+          (throw (IllegalArgumentException. (format "Invalid reference scheme %s" scheme))))))))
+
+(defn read-only?
+  [dref]
+  (if (satisfies? IDurableRef dref)
+    (:read-only? (-props dref))
+    (recur (reference dref))))
 
 (defn uri
-  "Returns the URI of the durable ref."
+  "Returns the URI of the ref."
   [dref]
-  (condp instance? dref
-    DurableRef (.-uri ^DurableRef dref)
-    DurableValueRef (.-uri ^DurableValueRef dref)
-    (throw (IllegalArgumentException. (format "Cannot take uri of (%s)" (class dref))))))
+  (if (satisfies? IDurableRef dref)
+    (:uri (-props dref))
+    (recur (reference dref))))
 
 (defn overwrite!
-  "Writes the obj to the durable ref."
+  "Writes the obj to the mutable ref."
   ([dref obj]
    (overwrite! dref obj {}))
   ([dref obj opts]
-   (let [dref (reference dref)
-         uri (uri dref)
-         sub-uri (.getSchemeSpecificPart ^URI uri)
-         sub-scheme (.getScheme (URI. ^URI uri))]
-     (when (instance? DurableValueRef dref)
-       (throw (IllegalArgumentException. "Cannot overwrite a DurableValueRef.")))
-     (write-bytes! sub-uri (serialize-to obj sub-uri) (-> opts :scheme-opts (get sub-scheme))))
-   nil))
+   (if (satisfies? IDurableRef dref)
+     (do
+       (when (read-only? dref)
+         (throw (IllegalArgumentException. "Cannot overwrite a readonly ref.")))
+       (let [uri (uri dref)
+             sub-uri (.getSchemeSpecificPart ^URI uri)
+             sub-scheme (.getScheme (URI. ^URI uri))]
+         (write-bytes! sub-uri (serialize-to obj sub-uri) (-> opts :scheme-opts (get sub-scheme))))
+       nil)
+     (recur (reference dref) obj opts))))
 
 (defn delete!
   "Deletes a reference from storage.
 
-  Accepts anything that can be referenced via `reference`.
-
-  Will accept a DurableValueRef, but be aware that this will invalidate the ref
-  and cause subsequent deref calls on the ref to crash."
+  Accepts anything (apart from readonly refs) that can be referenced via `reference`."
+  ([dref]
+   (delete! dref {}))
   ([dref opts]
-   (let [dref (reference dref)
-         uri (uri dref)
-         sub-scheme (.getScheme (URI. (.getSchemeSpecificPart uri)))]
-     (delete-bytes! uri (-> opts :scheme-opts (get sub-scheme))))))
+   (if (satisfies? IDurableRef dref)
+     (do
+       (when (read-only? dref)
+         (throw (IllegalArgumentException. "Cannot delete a readonly ref")))
+       (let [uri (uri dref)
+             sub-scheme (.getScheme (URI. (.getSchemeSpecificPart uri)))]
+         (delete-bytes! uri (-> opts :scheme-opts (get sub-scheme)))))
+     (recur (reference dref) opts))))
 
 (defn deref
   "Attempts to derefence a durable reference and returns a value.
-  dref can be a durable ref, durable value ref, or anything accepted by `reference`.
+  dref can be a anything accepted by `reference`.
 
   May throw an error if in the case of a value ref, storage has been mutated. (override with the *perform-checksum-check*
   var), it may also throw in general if storage is unavailable or crashes for whatever reason."
