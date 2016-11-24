43b0cb5328bbd616596967102da2782286cffb6c
Dan Stone <danielstone@riverford.co.ukls>
2016-11-24 10:54:34 +0000

    license + readme and what not

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # durable-ref
 
-A Clojure library designed to ... well, that part is up to you.
-
 ## Usage
-
-FIXME
 
 ## License
 
-Copyright © 2016 FIXME
+Copyright © 2016 Riverford Organic Farmers Ltd
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the [3-clause license ("New BSD License" or "Modified BSD License").](http://github.com/riverford/durable-ref/blob/master/LICENSE)
--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject riverford/durable-ref "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Durable reference types"
+  :url "https://github.com/riverford/durable-ref/"
+  :license "https://github.com/riverford/durable-ref/blob/master/LICENSE"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/data.fressian "0.2.1"]
                                   [amazonica "0.3.77"]
