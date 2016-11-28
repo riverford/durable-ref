92f85bad6c95eed5925ef7c8bb6d1a404293311a
Dan Stone <danielstone@riverford.co.ukls>
2016-11-28 11:42:28 +0000

    bumped to 0.1.2 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riverford/durable-ref "0.1.1"
+(defproject riverford/durable-ref "0.1.2-SNAPSHOT"
   :description "Durable reference types"
   :url "https://github.com/riverford/durable-ref/"
   :license "https://github.com/riverford/durable-ref/blob/master/LICENSE"
